dc4ba40206597209e8cc7c9b591309eb9674d228
Shane Pearson <bubbafoley@gmail.com>
2026-09-20 19:58:01 -0500

    Add Vercel speed insights and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { inject } from "@vercel/analytics"
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import StartGame from './game/main';
 
 inject();
+injectSpeedInsights();
 
 document.addEventListener('DOMContentLoaded', () => {
     StartGame('game-container');
